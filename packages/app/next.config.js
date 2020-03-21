5c5878d614b58ded3c60734bed5977779365de4a
Julien <julien.tanay@gmail.com>
2020-03-21 16:02:28 +0100

    :wrench: Fix build for now

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,4 +1,7 @@
+const assetPrefix = process.env.BUILDING_FOR_NOW === "true" ? "/server" : "";
+
 module.exports = {
+  assetPrefix,
   env: {
     signallingServer: "https://localhost:9002/",
     broadcastServer: "https://localhost:9001/"
