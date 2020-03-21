db6aa30935f0909f66c8a533729602a7e62d40d4
Julien <julien.tanay@gmail.com>
2020-03-21 16:07:30 +0100

    :wrench: Fix build for now BIS

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,4 +1,4 @@
-const assetPrefix = process.env.BUILDING_FOR_NOW === "true" ? "/server" : "";
+const assetPrefix = process.env.BUILDING_FOR_NOW === "true" ? "/app" : "";
 
 module.exports = {
   assetPrefix,
