cec5473a4fd016155bb1e8da77e6d1ca51c34aea
Julien Tanay <julien.tanay@gmail.com>
2020-03-24 16:34:27 +0100

    :sparkles: Add base offline support

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,8 +1,33 @@
 require("dotenv").config();
 
-module.exports = {
+const withOffline = require("next-offline");
+
+module.exports = withOffline({
   env: {
     signallingServer: process.env.signallingServer,
     broadcastServer: process.env.broadcastServer
+  },
+  target: "serverless",
+  transformManifest: manifest => ["/"].concat(manifest),
+  generateInDevMode: true,
+  workboxOpts: {
+    swDest: "static/service-worker.js",
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "https-calls",
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }
+    ]
   }
-};
+});
